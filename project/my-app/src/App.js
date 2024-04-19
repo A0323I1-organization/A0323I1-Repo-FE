496467d62ae1cc541ebedc5dacfa3af02186f53a
Tran Le Quang Trieu <trieutran025@gmail.com>
2024-04-19 19:38:34 +0700

    trieuTLQ

--- project/my-app/src/App.js
+++ project/my-app/src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+// import {BrowserRouter,Route,Routes} from "react-router-dom"
+import {BrowserRouter,Route,Routes} from "react-router-dom"
 import 'react-toastify/dist/ReactToastify.css';
+import Create from './components/employee/create';
 function App() {
   return (
     <>
+    
+    <BrowserRouter>
+        <Routes>
+        
 
+        <Route path='/create' element={<Create/>}></Route>
+
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
