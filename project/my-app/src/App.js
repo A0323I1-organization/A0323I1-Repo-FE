b68a4b6827b0ee8d339c4152daa86c68ecf25933
thanhngan <thanhngan19803@gmail.com>
2024-04-24 19:32:39 +0700

    Ngan

--- project/my-app/src/App.js
+++ project/my-app/src/App.js
@@ -5,15 +5,18 @@ import {BrowserRouter,Route,Routes} from "react-router-dom"
 import 'react-toastify/dist/ReactToastify.css';
 import Create from './components/employee/create';
 import Dasboard from './components/dashboard/dashboard';
+import ListEmployee from "./components/employee/ListEmployee";
+import DetailEmployee from "./components/employee/DetailEmployee";
 function App() {
   return (
     <>
     <BrowserRouter>
         <Routes>
-        
+
 
         <Route path='/create' element={<Create/>}></Route>
-
+            <Route path="/employee" element={<ListEmployee/>}></Route>
+            <Route path="/employee/detail/:id" element={<DetailEmployee/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
