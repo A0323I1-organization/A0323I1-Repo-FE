872ef7568731e6e7a41392abbc0ce96f39fe1752
vanphuc23 <phucnvp15072k3@gmail.com>
2024-04-24 20:01:11 +0700

    phuc

--- project/my-app/src/App.js
+++ project/my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import 'react-toastify/dist/ReactToastify.css';
 import Create from './components/employee/create';
+import Dasboard from './components/dashboard/dashboard';
 import SearchList from "./components/movie/searchList/SearchList";
 import DSF from "./components/movie/homePage/DSF";
 import UpcomingMovie from "./components/movie/upcomingMovie/UpcomingMovie";
